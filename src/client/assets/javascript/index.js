d3e2e73974ee7a27c79b399408a8ee77fa1c3499
Pablo Rodriguez Montalvo <pablo.rodriguez@e-intelligent.es>
2020-09-24 15:12:54 +0200

    wtf, i wrote "let" mal

--- src/client/assets/javascript/index.js
+++ src/client/assets/javascript/index.js
@@ -1,6 +1,6 @@
 // PROVIDED CODE BELOW (LINES 1 - 80) DO NOT REMOVE
 // The store will hold all information needed globally
-letflet store = {
+let store = {
   track_id: undefined,
   player_id: undefined,
   race_id: undefined,
